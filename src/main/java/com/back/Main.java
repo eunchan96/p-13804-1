9b16e5c11f01b3909ef97f028cf14fa84ee69b8e
eunchan96 <taegwon02@gmail.com>
2025-06-02 10:37:32 +0900

    labs1

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -1,7 +1,24 @@
 package com.back;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        labs1();
+    }
+
+    public static void labs1(){
+        String input = """
+                등록
+                나의 죽음을 적들에게 알리지 말라.
+                """;
+
+        Scanner scanner = new Scanner(input);
+        String cmd = scanner.nextLine();
+        String content = scanner.nextLine();
+
+        System.out.println("명령어: " + cmd);
+        System.out.println("내용: " + content);
 
     }
 }
